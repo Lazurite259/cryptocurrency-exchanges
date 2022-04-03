287615f9509a4ada9e04bd658d9e32988c92d589
Lazurite259 <73679778+Lazurite259@users.noreply.github.com>
2022-04-03 15:34:26 -0700

    Create ExchangeList.js

---
+++ src/components/ExchangeList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ExchangeRow from "./ExchangeRow";
+
+const titles = ["Exchange", "Country", "Website", "Trust Rank"]
+
+const ExchangeList = ({ exchanges }) => {
+    return (
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    {titles.map((title, i) => (
+                        <td key={i}>{title}</td>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {exchanges.map((exchange) => (
+                    <ExchangeRow key={exchange.id} exchange={exchange} />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default ExchangeList;
