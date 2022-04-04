8ef516d9b614e32c6bb03fe90b837a76791024dd
Lazurite259 <73679778+Lazurite259@users.noreply.github.com>
2022-04-03 21:07:26 -0700

    modify format

--- src/components/ExchangeRow.js
+++ src/components/ExchangeRow.js
@@ -11,11 +11,8 @@ const ExchangeRow = ({ exchange }) => {
                 <img src={exchange.image} alt={exchange.name} className="rounded img-fluid mx-3" style={{ width: "10%" }} />
                 <span>{exchange.name}</span>
             </td>
-
             <td>{exchange.country}</td>
-
             <td><a href={exchange.url}>{exchange.url}</a></td>
-
             <td className="text-center">{exchange.trust_score_rank}</td>
         </tr>
     );
