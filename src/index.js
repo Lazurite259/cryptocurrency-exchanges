a09fd3619266b82e9b750fc3b8628e4a0cd3bf5e
Lazurite259 <73679778+Lazurite259@users.noreply.github.com>
2022-04-03 21:05:44 -0700

    add Routes to access multi-page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
+import Exchange from './components/Exchange';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/:exchange" element={<Exchange />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
