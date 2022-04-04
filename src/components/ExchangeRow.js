ddf535440724ca4f5f3e90a1a10e7798649f4c78
Lazurite259 <73679778+Lazurite259@users.noreply.github.com>
2022-04-03 18:21:29 -0700

    change logo size and centered the rank

--- src/components/ExchangeRow.js
+++ src/components/ExchangeRow.js
@@ -4,12 +4,15 @@ const ExchangeRow = ({ exchange }) => {
     return (
         <tr>
             <td>
-                <img src={exchange.image} />
+                <img src={exchange.image} alt={exchange.name} className="rounded img-fluid mx-3" style={{ width: "10%" }} />
                 <span>{exchange.name}</span>
             </td>
+
             <td>{exchange.country}</td>
+
             <td><a href={exchange.url}>{exchange.url}</a></td>
-            <td>{exchange.trust_score_rank}</td>
+
+            <td className="text-center">{exchange.trust_score_rank}</td>
         </tr>
     );
 };
